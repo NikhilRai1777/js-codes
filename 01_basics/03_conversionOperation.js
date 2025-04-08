9a803188b26c8acf20640e323c50d9c62888f34d
Nikhil Rai <149245962+NikhilRai1777@users.noreply.github.com>
2025-04-08 12:19:17 +0000

    conversion Of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "nikhil"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+let isLoggedIn = 1;
+let booleanIsLogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLogedIn);
+console.log(typeof booleanIsLogedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "nikhil" => true
+
