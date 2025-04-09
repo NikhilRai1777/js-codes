26edef30e174ed8e2529462948d954cb5eff09a2
Nikhil Rai <149245962+NikhilRai1777@users.noreply.github.com>
2025-04-09 15:00:38 +0000

    string methods js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,16 +1,28 @@
 const name = "nikhil"
 const repoCount = 10
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String("Nikhil")
+const gameName = new String("Nikhil-nr")
 
-console.log(gameName[0])
-console.log(gameName.length)
-console.log(typeof gameName)
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('l'))
-console.log(gameName.substring(0, 4));
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(typeof gameName)
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('l'))
+// console.log(gameName.substring(0, 4));
 
-const anotherValue = gameName.slice(-4, 4)
-console.log(anotherValue);
+// const anotherValue = gameName.slice(-4, 4)
+// console.log(anotherValue);
+
+
+// const newString = "   nikhil  "
+// console.log(newString);
+// console.log(newString.trim());
+
+const url = "https://nikhil.com/nikhil%20rai"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes("nikhil"))
+
+console.log(gameName.split('-'))
