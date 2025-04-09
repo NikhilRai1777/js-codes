e2368340373132a2437ee422389ebde26ea78030
Nikhil Rai <149245962+NikhilRai1777@users.noreply.github.com>
2025-04-09 09:54:40 +0000

    updated some notes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -24,3 +24,24 @@ console.log(typeof "Nikhil");
 console.log(typeof age);
 console.log(typeof null);   //object
 console.log(typeof undefined);  //undefined
+
+//***** Non-primitive data types *****
+//array, object, function
+
+
+/*Typeof operator result: 
+
+Primitive types:
+Number => number
+String => string
+Boolean => boolean
+null => object
+undefined => undefined
+symbol => symbol
+bigint => bigint
+
+Non-Primitive types: 
+array => object
+object => object
+function => function
+*/
