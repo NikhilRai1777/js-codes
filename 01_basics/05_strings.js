ee79ae358752d6f1c6cde4aa379493b8584cad3e
Nikhil Rai <149245962+NikhilRai1777@users.noreply.github.com>
2025-04-09 12:52:29 +0000

    strings methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "nikhil"
+const repoCount = 10
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Nikhil")
+
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(typeof gameName)
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('l'))
+console.log(gameName.substring(0, 4));
+
+const anotherValue = gameName.slice(-4, 4)
+console.log(anotherValue);
