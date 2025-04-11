39c1cd3d375b1ff5ab4252eeda0d5f2f411a7a3b
Nikhil Rai <149245962+NikhilRai1777@users.noreply.github.com>
2025-04-11 10:10:11 +0000

    Marhs and numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+/*
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(typeof balance)
+console.log(balance.toFixed(2))
+
+const anotherNumber = 124.8974
+console.log(anotherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+*/
+
+// ****************Maths******************
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.5))
+// console.log(Math.ceil(5.1))
+// console.log(Math.floor(5.9))
+// console.log(Math.pow(5, 2))
+
+// console.log(Math.min(2, 9, 4, 5, 7))
+// console.log(Math.max(2, 9, 4, 5, 7))
+
+
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
