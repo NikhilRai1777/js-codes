955a8f858650b48439c3982f2f45ab09e7fce1f2
Nikhil Rai <149245962+NikhilRai1777@users.noreply.github.com>
2025-04-25 18:29:11 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,4 +1,4 @@
-// const tinderUser = new Object()
+// const tinderUser = new Object()          //Singleton obj
 const tinderUser = {}
 
 tinderUser.id = "123abc";
@@ -18,5 +18,55 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userFullName.firstName);
+// console.log(regularUser.fullname.userFullName.firstName);
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 =  Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2,
+        email: "i@gmail.com"
+    },
+    {
+        id: 3,
+        email: "j@gmail.com"
+    },
+]
+
+// console.log(users[1].email);
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "nikhil"
+}
+
+// const {courseInstructor} = course
+// console.log(courseInstructor);
+
+//OR
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
